912f0ab48c881f4e16eb2ba39c66a4849a78f3d8
JosueVargasGuia <44180784+JosueVargasGuia@users.noreply.github.com>
2021-11-30 18:03:52 -0500

    v30/11/2021

--- subaru/src/pago/succespayment.js
+++ subaru/src/pago/succespayment.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 export default   function SuccesPayment() {     
   
     return (<>
         <div>
             <div className="shop-success">
                 <h4>Muchas gracias por su compra!</h4>
-                <p>En breve recibirás un correo electrónico con el detalle de tu compra, también puedes visualizar el estado de tu compra desde aquí.</p>
+                <p>En breve recibirás un correo electrónico con el detalle de tu compra, también puedes visualizar el estado de tu compra desde <Link to="/dashboard">aquí</Link>.</p>
             </div>
         </div>
     </>)
