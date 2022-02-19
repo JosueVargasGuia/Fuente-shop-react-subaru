64b968ec559e74b4bddb084964fb760c5bdb44aa
Javier Castro <javier.castro@meetliquid.com>
2022-02-19 00:35:13 -0500

    add tag picture in slider

--- subaru/src/carrucel/carrucel.js
+++ subaru/src/carrucel/carrucel.js
@@ -10,21 +10,33 @@ import { homepage } from "../service/ENUM";
 export default function Carrucel(props) {   
   /* <img alt="outlet"  src={window.location.origin +(homepage==undefined?"":"/"+homepage) +"/marcas/outlet.png"}></img> */ 
   let rowItem = props.marca.lstCarrucel.map((objImagen) => (
-    <img
-      className="container-Carousel-img"
-      key={objImagen.codigoCarrucel}
-      alt={ objImagen.srcImage}
-      src={window.location.origin +(homepage===undefined?"":"/"+homepage) + objImagen.srcImage}
-    ></img>
+    <picture>
+      {/* Imagen PC */}
+      <source srcset={window.location.origin + (homepage === undefined ? "" : "/" + homepage) + objImagen.srcImage} media="(min-width: 800px)" />
+      {/* Imagen Mobile */}
+      <source srcset="https://via.placeholder.com/600x330?text=IMG" media="(min-width: 768px)"/>
+      <img
+        className="container-Carousel-img"
+        key={objImagen.codigoCarrucel}
+        alt={ objImagen.srcImage}
+        srcset={window.location.origin +(homepage===undefined?"":"/"+homepage) + objImagen.srcImage}
+      ></img>
+      {/* <img
+        className="container-Carousel-img"
+        key={objImagen.codigoCarrucel}
+        alt={ objImagen.srcImage}
+        src={window.location.origin +(homepage===undefined?"":"/"+homepage) + objImagen.srcImage}
+      ></img> */}
+    </picture>
   ));
 
   return (
     <div className="container-Carousel-list">    
-    <div className="outlet">
-      <Link to="/outlet"> 
-        <img alt="outlet"   src={window.location.origin +(homepage===undefined?"":"/"+homepage) +"/marcas/outlet.png"} ></img> 
-      </Link> 
-    </div>
+      <div className="outlet">
+        <Link to="/outlet"> 
+          <img alt="outlet"   src={window.location.origin +(homepage===undefined?"":"/"+homepage) +"/marcas/outlet.png"} ></img> 
+        </Link> 
+      </div>
       <Carousel
         autoPlay={true}
         infiniteLoop={true}
